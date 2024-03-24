ea10b7ed3d248c151b6aca661801f10b47da5bda
Tirtha Sharma <tirthagenze121@gmail.com>
2024-03-24 17:50:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const spanButtons = document.querySelectorAll(".button");
+const changeBodyColor = document.querySelector("body");
+
+spanButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (event.target.id === "grey") {
+      changeBodyColor.style.backgroundColor = event.target.id;
+      changeBodyColor.style.color = "black";
+    } else if (event.target.id === "white") {
+      changeBodyColor.style.backgroundColor = event.target.id;
+      changeBodyColor.style.color = "black";
+    } else if (event.target.id === "purple") {
+      changeBodyColor.style.backgroundColor = event.target.id;
+      changeBodyColor.style.color = "white";
+    } else if (event.target.id === "yellow") {
+      changeBodyColor.style.backgroundColor = event.target.id;
+      changeBodyColor.style.color = "black";
+    } else if (event.target.id === "orange") {
+      changeBodyColor.style.backgroundColor = event.target.id;
+      changeBodyColor.style.color = "black";
+    } else if (event.target.id === "blue") {
+      changeBodyColor.style.backgroundColor = "rgba(0, 117, 171, 0.399)";
+      changeBodyColor.style.color = "black";
+    } else if (event.target.id === "green") {
+      changeBodyColor.style.backgroundColor = event.target.id;
+    } else if (event.target.id === "black") {
+      changeBodyColor.style.backgroundColor = event.target.id;
+      changeBodyColor.style.color = "white";
+    } else if (event.target.id === "indianred") {
+      changeBodyColor.style.backgroundColor = event.target.id;
+    } else {
+      changeBodyColor.style.backgroundColor = event.target.id;
+      changeBodyColor.style.color = "black";
+    }
+  });
+});
